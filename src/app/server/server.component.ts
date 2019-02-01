4da330db8cf48acb0e50c17d3f338a7d300febb2
EslamElkhafagy <eslamelkhafagy@gmail.com>
2019-02-01 22:32:29 +0200

    create first component called server component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+
+
+@Component({
+selector:'app-server', // selector to call this component in other html page
+templateUrl:'./server.component.html' // to select what is the spacific html for this component 
+})
+export class ServerComponent{
+
+
+
+
+
+}   
