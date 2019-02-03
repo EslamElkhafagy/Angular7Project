85ad0cce052409bbbd12033f0dd20adc6664d4ba
EslamElkhafagy <eslamelkhafagy@gmail.com>
2019-02-04 01:37:47 +0200

    finished some design and make Recipe model & ingredient model and show them in html in recipe list and shopping list

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,16 @@
+export class Recipe {
+
+    public name: String;
+    public description: string;
+    public imagePath: string;
+
+
+    constructor(name: string, description: string, imagePath: string) {
+this.name=name;
+this.description=description;
+this.imagePath=imagePath;
+    }
+
+
+
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { Ingredient } from './../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients:Ingredient[]=[
+    new Ingredient("ml7 w ay klam",10),
+    new Ingredient("ay 7aga brdo ",7)
+    ];
   constructor() { }
 
   ngOnInit() {
