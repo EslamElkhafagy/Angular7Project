b8995b3d32286b91b6ccb3773c9d6fe2a597a9b1
EslamElkhafagy <eslamelkhafagy@gmail.com>
2019-02-03 03:13:51 +0200

    simple exmple for *ngfor

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,9 +5,26 @@ import { Component } from '@angular/core';
 
 @Component({
 selector:'app-server', // selector to call this component in other html page
-templateUrl:'./server.component.html' // to select what is the spacific html for this component 
+templateUrl:'./server.component.html', // to select what is the spacific html for this component 
+styleUrls:['./server.component.css'] 
 })
 export class ServerComponent{
+
+
+severId : number =4200;
+serverStatus :string = 'serverIsOnline'; 
+
+count:number=0;
+testArray=['Eslam','Elkhafagy','Ahmed','sasa'];
+
+
+
+addName(){
+this.count++;
+this.testArray.push('newName'+this.count);
+
+
+}
 
 
 
